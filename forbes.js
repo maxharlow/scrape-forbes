eb822b706c73eb1a24336e5451d0ce8e0b190dcb
Max Harlow <max.harlow@ft.com>
2019-12-30 20:48:02 +0000

    Updates

--- forbes.js
+++ forbes.js
@@ -1,7 +1,7 @@
-const FS = require('fs')
-const Request = require('request')
-const Highland = require('highland')
-const CSVWriter = require('csv-write-stream')
+const Ix = require('ix')
+const Axios = require('axios')
+const PapaParse = require('papaparse')
+const FSExtra = require('fs-extra')
 
 const lists = [
     { type: 'person', year: 2017, uri: 'forbes-400' },               // American richest 400
@@ -28,29 +28,72 @@ const lists = [
     { type: 'person', year: 0, uri: 'rtrl' },                        // real-time American richest 400
 ]
 
-const http = Highland.wrapCallback((location, callback) => {
-    return Request(location, (error, response) => {
-        const failure = error ? error : (response.statusCode >= 400) ? new Error(response.statusCode) : null
-        callback(failure, response)
-    })
-})
-
-function locate(list) {
+async function request(location) {
+    const url = typeof location === 'object' ? location.url : location
+    const timeout = 30 * 1000
+    const instance = Axios.create({ timeout })
+    const response = await instance(location)
     return {
-        uri: 'http://www.forbes.com/ajax/list/data',
-        qs: list
+        url,
+        data: response.data,
+        passthrough: location.passthrough
+    }
+}
+
+function locate(item) {
+    return {
+        url: 'http://www.forbes.com/ajax/list/data',
+        params: item,
+        passthrough: item
     }
 }
 
 function parse(response) {
-    return JSON.parse(response.body)
+    return response.data.map(item => {
+        return {
+            name: item.name,
+            surname: item.lastName,
+            position: item.position,
+            rank: item.rank,
+            worth: item.worth,
+            age: item.age,
+            source: item.source,
+            industry: item.industry,
+            gender: item.gender,
+            country: item.country
+        }
+    })
 }
 
-lists.map(list => {
-    Highland([list])
-        .map(locate)
-        .flatMap(http)
-        .flatMap(parse)
-        .through(CSVWriter())
-        .pipe(FS.createWriteStream('forbes-' + list.uri + '.csv'))
-})
+function csv() {
+    let headerWritten = false
+    return function* (record) {
+        if (!headerWritten) {
+            const header = PapaParse.unparse([Object.keys(record)])
+            yield header + '\n'
+            headerWritten = true
+        }
+        const entry = PapaParse.unparse([Object.values(record)])
+        yield entry + '\n'
+    }
+}
+
+async function write(filename) {
+    await FSExtra.remove(filename)
+    return contents => FSExtra.appendFile(filename, contents)
+}
+
+async function run() {
+    await Ix.AsyncIterable.from(lists)
+        .forEach(async item => {
+            Ix.AsyncIterable.from([item])
+                .map(locate)
+                .map(request)
+                .flatMap(parse)
+                .flatMap(csv())
+                .forEach(await write(`forbes-${item.uri}.csv`))
+        })
+        .finally(() => console.log('Done!'))
+}
+
+run()
